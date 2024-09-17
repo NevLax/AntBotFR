a1141d2b8336bf038b0da8a00305e0d04f06a6c1
NevLax <qwegornikitin@gmail.com>
2024-09-17 17:41:11 +0400

    final Robot

--- game/Robot.py
+++ game/Robot.py
@@ -7,72 +7,29 @@ from game.consts import DEFAULT_IMAGE_SIZE
 
 class Robot:
     image_paths = {
-        'blue': 'images/blue_robot.png',
-        'red': 'images/red_robot.png',
-        'green': 'images/green_robot.png',
-        'orange': 'images/orange_robot.png'
+        0: 'images/blue_robot.png',     # blue
+        1: 'images/red_robot.png',      # red
+        2: 'images/green_robot.png',    # green
+        3: 'images/orange_robot.png'    # orange
     }
 
-    def __init__(self, color, pos, index, player, width=DEFAULT_IMAGE_SIZE[0], height=DEFAULT_IMAGE_SIZE[1]):
+    def __init__(self, pos, index, player, width=DEFAULT_IMAGE_SIZE[0], height=DEFAULT_IMAGE_SIZE[1]):
+        self.player = player
         self.number_img = None
-        self.color = color
         self.pos = pos
-        self.has_package = False
         self.package = None
         self.index = index
         self.set_number_image()
-        image = pygame.image.load(Robot.image_paths[color])
-        self.image = pygame.transform.scale(image, (width, height))
+        self.image = pygame.transform.scale(pygame.image.load(Robot.image_paths[player.id]), (width, height))
         self.rect = self.image.get_rect(topleft=((pos[0] + 1) * width, (pos[1] + 1) * height))
-        self.player = player
 
-    def move(self, direction, board, animation_steps=10):
-        """Движение роботов"""
-        x, y = self.pos
-        new_x, new_y = x, y
-        if direction == "up" and y > 0:
-            new_y = y - 1
-        elif direction == "down" and y < board.size - 1:
-            new_y = y + 1
-        elif direction == "left" and x > 0:
-            new_x = x - 1
-        elif direction == "right" and x < board.size - 1:
-            new_x = x + 1
-        else:
-            return False
-        if 0 <= new_x < board.size and 0 <= new_y < board.size:
-            if not board.is_occupied((new_x, new_y)):
-                target_cell = board[new_y][new_x]
-                if target_cell.color == 'r':
-                    logging.info(
-                        f"Robot {self.index} of Player {self.player.idx + 1} tried to move to a red cell at ({chr(ord('A') + new_x)}, {new_y + 1}). Move cancelled.")
-                    return False
-                if target_cell.target > 0 and (not self.package or self.package.number != target_cell.target):
-                    logging.info(
-                        f"Robot {self.index} of Player {self.player.idx + 1} tried to move to a target cell with "
-                        f"incompatible package at ({chr(ord('A') + new_x)}, {new_y + 1}). Move cancelled.")
-                    return False
-                self.animate_move(self.rect.copy(), new_x, new_y, animation_steps, board)
-                board.update_position(self.pos, (new_x, new_y))
-                self.pos = (new_x, new_y)
-                self.rect.topleft = ((new_x + 1) * DEFAULT_IMAGE_SIZE[0], (new_y + 1) * DEFAULT_IMAGE_SIZE[1])
-                logging.info(
-                    f"Player {self.player.idx + 1} moved robot {self.index} {direction} to ({chr(ord('A') + new_x)}, {new_y + 1}).")
-                # Execute additional actions after movement
-                if target_cell.target and self.package and target_cell.target == self.package.number:
-                    self.drop_package(target_cell)
-                if not self.has_package:
-                    if target_cell.color == 'a':
-                        below_cell = board[new_y + 1][new_x]
-                        if below_cell.color == 'r' and below_cell.package and not below_cell.package.picked_up:
-                            self.pick_package(below_cell.package, board)
-                if self.package:
-                    self.package.set_position(self.pos)
-                return True
-        return False
+    def move(self, new_pos):
+        self.pos = new_pos
+        self.animate_move()
 
-    def animate_move(self, old_rect, new_x, new_y, steps, board):
-        """Анимация движения: Анимация движения робота, чтобы робот двигался плавно"""
+    def animate_move(self, steps=10):
+        new_x, new_y = self.pos
+        old_rect = self.rect.copy()
         step_x = ((new_x + 1) * DEFAULT_IMAGE_SIZE[0] - old_rect.x) / steps
         step_y = ((new_y + 1) * DEFAULT_IMAGE_SIZE[1] - old_rect.y) / steps
         for i in range(steps):
@@ -82,34 +39,26 @@ class Robot:
             pygame.display.flip()
             pygame.time.delay(5)
 
-    def pick_package(self, package, board):
-        """Робот поднял посылку с зеленой клетки"""
-        self.has_package = True
+    def pick_package(self, package) -> Package:
         self.package = package
         package.pick_up()
         logging.info(
-            f"Robot {self.index} of Player {self.player.idx + 1} picked up package with number {package.number} at position ({chr(ord('A') + (self.pos[0]))}, {self.pos[1] + 1}).")
+            f"Robot {self.index} of Player {self.player.id + 1} picked up package with number {package.number} at position ({chr(ord('A') + (self.pos[0]))}, {self.pos[1] + 1}).")
         new_package = Package(package.pos)
         new_package.number = random.randint(1, 9)
-        board[package.pos[1]][package.pos[0]].package = new_package
+        return new_package
 
     def drop_package(self, cell):
-        """Робот сдал посылку с соответствующим номером в пункт приема отмеченной цифрой"""
-        if not self.has_package:
+        if not self.package:
             return False
         logging.info(
             f"Robot {self.index} of Player {self.player.idx + 1} dropped package with number {self.package.number} at position ({chr(ord('A') + (self.pos[0]))}, {self.pos[1] + 1}).")
-        self.player.increase_score(self.package.number, self.player.game_manager)
         self.package.drop_off()
         self.package = None
-        self.has_package = False
         cell.package = None
         return True
 
-    def increase_score(self, player, ):
-
-    def RobotAnimator(self, screen):
-        """Анимирует робота"""
+    def robot_animator(self, screen):
         screen.blit(self.image, self.rect)
         if self.number_img:
             number_pos = (
